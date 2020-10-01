3952e95bf0fde2e8b67b01c8aebef67cad3aa334
sidwahi <git@cma-ih.com>
2020-10-01 22:44:59 +0400

    delete locks

---
+++ pages/video/index.vue
@@ -0,0 +1,50 @@
+<template>
+ 
+  <div class="container">
+    <div>
+    
+      
+      <h1 class="title">
+        brightcove
+      </h1>
+      
+      </div>
+<div id="player-container"></div>
+
+<script src="brightcove-player-loader.js"></script>
+<script>
+  brightcovePlayerLoader({
+    refNode: '#player-container',
+    accountId: '1571595826001',
+    playerId: 'c0AOssA7',
+
+    embedOptions: {
+    responsive: {
+      aspectRatio: '16:9',
+      maxWidth: '100%'
+    }
+  },
+
+    videoId: 6194583373001,
+   
+  });
+</script>
+
+</div>
+
+
+</template>
+
+<script>
+export default {};
+
+</script>
+
+<script>
+////brightcovePlayerLoader({
+  ///refNode: someElement,
+  ///accountId: '1571595826001'
+////});
+
+</script>
+
